6cec2f5b8540937bf8f0da7d42b2a66be5727f8b
Shaleen Jain <shaleen@jain.sh>
2019-09-06 16:32:49 +0530

    add method whitelist

--- src/handler.ts
+++ src/handler.ts
@@ -10,6 +10,45 @@ export async function handleRequest(request: Request) {
   const url = new URL("https://compensation-wrestling-golf-idle.trycloudflare.com");
   const headers = new Headers();
   headers.append("content-type", "application/json;charset=UTF-8");
+  const allowedMethods = [
+    "web3_clientVersion",
+    "web3_sha3",
+    "net_version",
+    "net_peerCount",
+    "net_listening",
+    "eth_protocolVersion",
+    "eth_syncing",
+    "eth_mining",
+    "eth_hashrate",
+    "eth_gasPrice",
+    "eth_accounts",
+    "eth_blockNumber",
+    "eth_getBalance",
+    "eth_getStorageAt",
+    "eth_getTransactionCount",
+    "eth_getBlockTransactionCountByHash",
+    "eth_getBlockTransactionCountByNumber",
+    "eth_getUncleCountByBlockHash",
+    "eth_getUncleCountByBlockNumber",
+    "eth_getCode",
+    "eth_sendRawTransaction",
+    "eth_call",
+    "eth_estimateGas",
+    "eth_getBlockByHash",
+    "eth_getBlockByNumber",
+    "eth_getTransactionByHash",
+    "eth_getTransactionByBlockHashAndIndex",
+    "eth_getTransactionByBlockNumberAndIndex",
+    "eth_getTransactionReceipt",
+    "eth_pendingTransactions",
+    "eth_getUncleByBlockHashAndIndex",
+    "eth_getUncleByBlockNumberAndIndex",
+    "eth_getLogs",
+    "eth_getWork",
+    "eth_submitWork",
+    "eth_submitHashrate",
+    "eth_getProof",
+  ];
 
   const httpClient = new HttpClient(url, <RequestInit> {
     method: "POST",
@@ -26,6 +65,10 @@ export async function handleRequest(request: Request) {
 
   let body = await request.json();
   console.log(body);
+
+  if (!body.method || !allowedMethods.includes(body.method)) {
+    return new Response("Method not allowed", { status: 403 });
+  }
 
   return httpClient.fetch("/", <RequestInit> {
     body: JSON.stringify(body),
